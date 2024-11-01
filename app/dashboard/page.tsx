6236f6706b5122dccbf958b87df758eda0a36569
sanmusen <sen.zeng@coolkit.cn>
2024-11-01 09:29:52 +0800

    feat: 新增基本框架

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,10 @@
-export default function Page() {
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: 'Home',
+};
+
+export default async function Page() {
+    await new Promise(resolve => setTimeout(resolve, 5000));
     return <p>Dashboard Page</p>;
 }
